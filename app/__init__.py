146c326e852b3650d554fe9640615483c760229f
leerobertdyer <leerobertdyer@gmail.com>
2023-11-29 21:10:10 -0500

    scrape working

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,12 @@ app.config.from_object(Config)
 
 @app.route('/')
 def home():
-    URL = "https://realpython.github.io/fake-jobs/"
-    page = requests.get(URL)
-    print(page.text)
+    URL = "https://www.thegreyeagle.com/calendar/"
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
+    resp = requests.get(URL, headers=headers)
+    if resp.status_code == 200:
+        # print(resp.text)
+        pass
+    else:
+        print(f"Failed to retrieve page. Status code: {resp.status_code}")
     return render_template('home.html')
