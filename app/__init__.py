99c477f92b4b87f4f83380371385b51aa7816770
leerobertdyer <leerobertdyer@gmail.com>
2023-11-29 20:44:38 -0500

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
