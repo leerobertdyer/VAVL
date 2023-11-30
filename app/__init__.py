771c1778e413fcf5d19653c0262b951fb805ac01
leerobertdyer <leerobertdyer@gmail.com>
2023-11-29 20:54:10 -0500

    for safety

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 from config import Config
+import requests
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 @app.route('/')
 def home():
+    URL = "https://realpython.github.io/fake-jobs/"
+    page = requests.get(URL)
+    print(page.text)
     return render_template('home.html')
