f0e2f20ed6ce06be0df97a91f4f4580b956415c6
leerobertdyer <leerobertdyer@gmail.com>
2023-12-03 11:38:31 -0500

    functional db and server logic

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from app import app, db
 import requests
 from bs4 import BeautifulSoup
 from .models import Event
-import datetime
+from datetime import datetime
 
 @app.route('/')
 def home():
@@ -18,93 +18,107 @@ def eagle():
     soup = BeautifulSoup(resp.content, "html.parser")
     
     eagleEvents = []
-    
+    showYear = ''
     if resp.status_code == 200:
-        showDivs = soup.find_all("div", class_="rhpSingleEvent")
-        for show in showDivs:
-            try:
-                showDay = show.find(id="eventDate").text 
-                showYear = show.find("span", class_="rhp-events-list-separator-month").text[-4:]
-                showDate = showDay + " " + showYear
-            except:
-                showDate = "Date Not Found"
-            try:
-                showTitle = show.find(id="eventTitle").find('h2').text
-            except:
-                showTitle = "Title not found"
-            try:
-                showImage = show.find("img", class_="eventListImage")['src'] 
-            except:
-                showImage = "app/static/sad.jpg"
-            try:
-                showTickets = show.find(id="ctaspan-44637").find("a")['href']
-            except:
-                showTickets = "Tickets Not Found"
-            eagleEvents.append({
-                "showDate": showDate, 
-                "showTitle": showTitle, 
-                "showImage": showImage, 
-                "showTickets": showTickets})
+        mainDiv = soup.find("div", class_="rhp-desktop-list")
+        for child in mainDiv.children:
+            if child.name == "span":
+               showYear = child.span.text[-4::].strip()
+            elif child.name == "div":
+                try:
+                    showDay = child.find(id="eventDate").text.strip()
+                    showDateStr = showDay + " " + showYear
+                    try:
+                        showDate = datetime.strptime(showDateStr, "%a, %b %d %Y")
+                    except:
+                        showDate = datetime.strptime(showDateStr, "%a, %B %d %Y")
+                except:
+                    showDate = "Date Not Found"
+                try:
+                    showTitle = child.find(id="eventTitle").find('h2').text
+                except:
+                    showTitle = "Title not found"
+                try:
+                    showImage = child.find("img", class_="eventListImage")['src'] 
+                except:
+                    showImage = "app/static/sad.jpg"
+                try:
+                    showTickets = child.find(id="eventTitle")["href"]
+                except:
+                    showTickets = "Tickets Not Found"
+                eagleEvents.append({
+                    "showDate": showDate, 
+                    "showTitle": showTitle, 
+                    "showImage": showImage, 
+                    "showTickets": showTickets})
         for show in eagleEvents:
-            new_event = Event(
-                venue = "Grey Eagle",
-                title = show["showTitle"],
-                show_date = datetime.strptime(show["showDate"], "%a, %b %d %Y"),
-                tickets = show["showTickets"],
-                image = show["showImage"],
-            )
-            db.session.add(new_event)
-            db.session.commit()
+            existing_event = Event.query.filter_by(title=show["showTitle"], show_date=show["showDate"]).first()
+            if existing_event is None:
+                new_event = Event(
+                    venue = "Grey Eagle",
+                    title = show["showTitle"],
+                    show_date = show["showDate"],
+                    tickets = show["showTickets"],
+                    image = show["showImage"],
+                )
+                db.session.add(new_event)
+                db.session.commit()
     else:
         print(f"Failed to retrieve page. Status code: {resp.status_code}")
     return eagleEvents
 
 @app.route('/peel')
 def peel():
-    
     url = "https://theorangepeel.net/events/?view=list"
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
     resp = requests.get(url, headers=headers)
     soup = BeautifulSoup(resp.content, "html.parser")
     
     peelEvents = []
-    
+    showYear = ''
     if resp.status_code == 200:
-        showDivs = soup.find_all("div", class_="rhpSingleEvent")
-        for show in showDivs:
-            try:
-                showDay = show.find(id="eventDate").text 
-                showYear = show.find('span', class_="rhp-events-list-separator-month").text[-4:]
-                showDate = showDay + " " + showYear
-            except:
-                showDate = "Date Not Found"
-            try:
-                showTitle = show.find(id="eventTitle").find('h2').text
-            except:
-                showTitle = "Title not found"
-            try:
-                showImage = show.find("img", class_="eventListImage")['src'] 
-            except:
-                showImage = "app/static/sad.jpg"
-            try:
-                showTickets = show.find("div", class_="rhp-event-list-cta").find("a")['href']
-            except:
-                showTickets = "Tickets Not Found"
-            peelEvents.append({
-                "showDate": showDate, 
-                "showTitle": showTitle, 
-                "showImage": showImage, 
-                "showTickets": showTickets})
+        mainDiv = soup.find("div", class_="rhp-desktop-list")
+        for child in mainDiv:
+            if child.name == "span":
+                showYear = child.span.text[-4::].strip()
+            elif child.name == "div":
+                try:
+                    showDay = child.find(id="eventDate").text.strip() 
+                    showDateStr = showDay + " " + showYear
+                    if showDateStr[5:9].upper() == "SEPT":
+                        showDateStr = showDateStr[0:8] + showDateStr[9::]
+                    showDate = datetime.strptime(showDateStr, "%a, %b %d %Y")
+                except:
+                    showDate = "Date Not Found"
+                try:
+                    showTitle = child.find(id="eventTitle").find('h2').text.strip()
+                except:
+                    showTitle = "Title not found"
+                try:
+                    showImage = child.find("img", class_="eventListImage")['src'] 
+                except:
+                    showImage = "app/static/sad.jpg"
+                try:
+                    showTickets = child.find(id="eventTitle")["href"]
+                except:
+                    showTickets = "Tickets Not Found"
+                peelEvents.append({
+                    "showDate": showDate, 
+                    "showTitle": showTitle, 
+                    "showImage": showImage, 
+                    "showTickets": showTickets})
         for show in peelEvents:
-            new_event = Event(
-                venue = "Orange Peel",
-                title = show["showTitle"],
-                show_date = datetime.strptime(show["showDate"], "%a, %b %d %Y"),
-                tickets = show["showTickets"],
-                image = show["showImage"],
-            )
-            db.session.add(new_event)
-            db.session.commit()
+            existing_event = Event.query.filter_by(title=show["showTitle"], show_date=show["showDate"]).first()
+            if existing_event is None:
+                new_event = Event(
+                    venue = "Orange Peel",
+                    title = show["showTitle"],
+                    show_date = show["showDate"],
+                    tickets = show["showTickets"],
+                    image = show["showImage"],
+                )
+                db.session.add(new_event)
+                db.session.commit()
     else:
         print(f"Failed to retrieve page. Status code: {resp.status_code}")
     return peelEvents
@@ -126,7 +140,9 @@ def rabbit():
         for show in showDivs:
             try:
                 dateAndTime = show.find("span", class_="tribe-event-date-start").text 
-                showDate = dateAndTime[:-9]
+                showDateStr = dateAndTime[:-9].strip()
+                print(showDateStr)
+                showDate = datetime.strptime(showDateStr, "%B %d, %Y")
             except:
                 showDate = "Date/time Not Found"
             try:
@@ -138,7 +154,7 @@ def rabbit():
             except:
                 showImage = "app/static/sad.jpg"
             try:
-                showTickets = show.find("div", class_="prc-tribe-list-links").find("a")['href']
+                showTickets = show.find("h3", class_="tribe-events-calendar-list__event-title").find("a")['href']
             except:
                 showTickets = "Tickets Not Found"
             rabbitEvents.append({
@@ -147,15 +163,17 @@ def rabbit():
                 "showImage": showImage, 
                 "showTickets": showTickets})
         for show in rabbitEvents:
-            new_event = Event(
-                venue = "Rabbit Rabbit",
-                title = show["showTitle"],
-                show_date = datetime.strptime(show["showDate"], "%B %d, %Y"),
-                tickets = show["showTickets"],
-                image = show["showImage"]
-            )
-            db.session.add(new_event)
-            db.session.commit()
+            existing_event = Event.query.filter_by(title=show["showTitle"], show_date=show["showDate"]).first()
+            if existing_event is None:
+                new_event = Event(
+                    venue = "Rabbit Rabbit",
+                    title = show["showTitle"],
+                    show_date = show["showDate"],
+                    tickets = show["showTickets"],
+                    image = show["showImage"]
+                )
+                db.session.add(new_event)
+                db.session.commit()
     else:
         print(f"Failed to retrieve page. Status code: {resp.status_code}")
     return rabbitEvents
@@ -171,13 +189,22 @@ def cherokee():
     
     if resp.status_code == 200:
         showDivs = soup.find_all("div", class_="list-view")
-        
         for show in showDivs:
             try:
                 showDay = show.find("div", class_="event-date").text.strip()
+                if len(showDay) < 5:
+                    print('Too Short!')
+                    alternateDate = show.find("div", class_="event-subtitle").text.strip()
+                    print('alt date: ', alternateDate)
+                    for char in alternateDate:
+                        if char.isdigit():
+                            showDay += " " + char
+                            break
                 showDate = showDay + " 2024"
+                print('final showDate: ', showDate)
+                            
             except:
-                showDate = "Jan 1, 1955"
+                showDate = "Date Not Found"
             try:
                 showTitle = show.find("div", class_="event-details").find("h3").text
             except:
@@ -196,15 +223,17 @@ def cherokee():
                 "showImage": showImage, 
                 "showTickets": showTickets})
         for show in cherokeeEvents:
-            new_event = Event(
-                venue = "Harrah's Cherokee",
-                title = show["showTitle"],
-                show_date = datetime.strptime(show["showDate"], "%b %d, %Y"),
-                tickets = show["showTickets"],
-                image = show["showImage"]
-            )
-            db.session.add(new_event)
-            db.session.commit()
+            existing_event = Event.query.filter_by(title=show["showTitle"], show_date=show["showDate"]).first()
+            if existing_event is None:
+                new_event = Event(
+                    venue = "Harrah's Cherokee",
+                    title = show["showTitle"],
+                    show_date = show["showDate"],
+                    tickets = show["showTickets"],
+                    image = show["showImage"]
+                )
+                db.session.add(new_event)
+                db.session.commit()
     else:
         print(f"Failed to retrieve page. Status code: {resp.status_code}")
     return cherokeeEvents
@@ -228,11 +257,12 @@ def salvage():
                 showMonth = show.find("div", class_="event-list-month").text.strip()
                 showNum = show.find("div", class_="event-list-number").text.strip()
                 showYear = show.find("div", class_="event-list-year").text.strip()
-                showDate = f"{showDay} {showMonth} {showNum} {showYear}"
+                showDateStr = f"{showDay} {showMonth} {showNum} {showYear}"
+                showDate = datetime.strptime(showDateStr, "%a %b %d %Y")
             except:
                 showDate = "Date/time Not Found"
             try:
-                showTitle = show.find("div", class_="event-list-title").text
+                showTitle = show.find("div", class_="event-list-title").text.strip()
             except:
                 showTitle = "Title not found"
             try:
@@ -240,7 +270,7 @@ def salvage():
             except:
                 showImage = "app/static/sad.jpg"
             try:
-                showTickets = show.find("a", class_="event-list-button buy")['href']
+                showTickets = show.find("div", class_="event-list-titles").find("a")["href"]
             except:
                 showTickets = "Tickets Not Found"
             salvageEvents.append({
@@ -252,7 +282,7 @@ def salvage():
             new_event = Event(
                 venue = "Salvage Station",
                 title = show["showTitle"],
-                show_date = datetime.strptime(show["showDate"], "%a, %b, %d, %Y"),
+                show_date = show["showDate"],
                 tickets = show["showTickets"],
                 image = show["showImage"]
             )
