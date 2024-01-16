1018bccfffd650b6eee5123f27901508545f4e72
leerobertdyer <leerobertdyer@gmail.com>
2024-01-16 13:29:42 -0500

    added render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+STORAGE_DIR=/opt/render/project/.render
+
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src
+else
+  echo "...Using Chrome from cache"
+fi
+
+
