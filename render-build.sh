fcaea274c00ac9cfeb72ae92e87ee7b9de5b6bf1
leerobertdyer <leerobertdyer@gmail.com>
2024-01-16 15:43:05 -0500

    changed build.sh should log to build...

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-set -e
+
+exec &> /opt/render/project/build.log
 
 STORAGE_DIR=/opt/render/project/.render
 
@@ -7,14 +7,10 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  echo "INSTALLING TO THIS DIRECTORY: $STORAGE_DIR/chrome"
   rm ./google-chrome-stable_current_amd64.deb
   cd $HOME/project/src
 else
   echo "...Using Chrome from cache"
 fi
-
-
